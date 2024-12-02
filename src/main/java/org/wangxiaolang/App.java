6205f5b9c5717b5b21d11ffc27f901bdc3b2853f
wangzuoyu <zuoyuwang@creditease.cn>
2024-12-02 11:20:48 +0800

    init

---
+++ src/main/java/org/wangxiaolang/App.java
@@ -0,0 +1,13 @@
+package org.wangxiaolang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
